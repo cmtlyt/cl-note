e52b4f9a51740f2932599bd000de0ffcd86a1148
cmtlyt <cmtlyt@users.noreply.github.com>
2024-06-21 04:47:27 +0000

    deploy: 065f22bc5c61644b50b7c5f2d5ba4d02bb58a7a6

--- assets/Edit-DyZKNNzd.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Dyse7RrW.js";import"./react-vendor-DF0UQ0u3.js";import"./__commonjsHelpers__-Cpj98o6Y.js";function d(){return t("div",{children:"Edit"})}export{d as default};
---
+++ assets/User-B-F7ByDj.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C0y08yPM.js";import"./react-vendor-BC_lhG4F.js";import"./__commonjsHelpers__-Cpj98o6Y.js";function o(){return r("div",{children:"User"})}export{o as default};
