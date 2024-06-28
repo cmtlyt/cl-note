8151d086f1b3a29bfa2e99f162efe4c67aaa2627
cmtlyt <cmt01964896@taobao.com>
2024-06-28 17:33:54 +0800

    fix: 移除无效代码

--- src/api/bill.ts
+++ src/api/bill.ts
@@ -1,15 +1,11 @@
 import { http } from './request';
 
-import { updateStatistics } from '@/components/AmountStatisticsBox';
 import { CreateBill, GetBillList } from '@/types/api';
 
 export const getBillList: GetBillList = (query) => {
   return http.get('/bill/list', { params: query });
 };
 
-export const createBill = (async (data) => {
-  return http.post('/createBill', data).then((res) => {
-    updateStatistics();
-    return res;
-  });
-}) as CreateBill;
+export const createBill: CreateBill = (data) => {
+  return http.post('/createBill', data);
+};
